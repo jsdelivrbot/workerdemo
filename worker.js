cb21b13af45641d70eb8db9d1261146d066e9ee4
ritz <ritz@ritz.com>
2017-11-18 18:39:01 +0530

    Commit workeer

--- worker.js
+++ worker.js
@@ -1,14 +1,22 @@
 var schedule = require('node-schedule');
+var fs = require('fs');
 
 var APP = {
   scheduleJob: function() {
     // This rule is standard cron syntax for once per minute.
     // See http://stackoverflow.com/a/5398044/1252653
-    rule = '* * * * *'
+    rule = '* * * * *';
 
     // Kick off the job
     var job = schedule.scheduleJob(rule, function() {
       console.log('ping!')
+      fs.writeFile("./test", "Hey there!", function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    
+        console.log("The file was saved!");
+      }); 
     });
   },
 
@@ -17,6 +25,8 @@ var APP = {
   }
 };
 
+
 (function(){
+  console.log("HERE");
   APP.init();
 })();
